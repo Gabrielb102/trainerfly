bedb8fd1753a6adad322790cc37f34e90bf12672
Gabriel Burgos <gabrielb102@gmail.com>
2025-06-20 09:32:46 -0700

    # 0.5.2 - Fix side menu positioning
    Side menu was at inconvenient z-index, now it is no longer covered by the header's shadow.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,7 +3,7 @@ import Wordmark from '@/components/svg/Wordmark.vue'
 </script>
 
 <template>
-  <header class="flex justify-between gap-0 w-full h-32 p-4 bg-white shadow-md/30 z-100">
+  <header class="flex justify-between gap-0 w-full h-32 p-4 bg-white shadow-md/30 z-40">
     <div class="flex justify-start items-center h-full">
       <Wordmark alt="trainerfly" class="h-auto w-60 ml-4" pathClass="fill-[#212020]"/>
     </div>
