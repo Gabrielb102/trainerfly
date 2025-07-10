a0ba3092b4c63823fef421dec92fdc87fa0f12a1
Gabriel Burgos <gabrielb102@gmail.com>
2025-07-09 17:17:00 -0700

    Refactor CategoryCard component to improve layout and styling. Adjusted padding and alignment for better visual consistency, and added line-clamping to the category name for improved readability.

--- frontend/src/components/CategoryCard.vue
+++ frontend/src/components/CategoryCard.vue
@@ -45,15 +45,16 @@ const viewChildCategories = () => {
 </script>
 
 <template>
-    <div class="w-full aspect-square min-h-20 flex flex-col items-center justify-center gap-2 p-4 aspect-square shadow-md/40 rounded-xl cursor-pointer bg-gray-100 hover:bg-gray-200 transition-colors"
+    <div class="w-full aspect-square min-h-20 flex flex-col items-center justify-center gap-2 py-4 px-2 aspect-square shadow-md/40 rounded-xl cursor-pointer bg-gray-100 hover:bg-gray-200 transition-colors"
         @click="selectCategory">
         <div class="w-full h-full flex items-center justify-center">
             <UIcon :name="category.icon ? `fa6-solid:${category.icon}` : 'fa6-solid:circle-question'"
                 class="size-10 text-gray-400" />
         </div>
-        <div class="w-full h-fit flex items-center justify-center gap-2">
-            <span class="h-fit w-full text-md leading-5 font-bold text-center uppercase text-black">{{ category.name
-                }}</span>
+        <div class="w-full h-fit flex items-center justify-center gap-0">
+            <span class="h-fit w-full text-center text-md leading-5 font-bold uppercase text-black line-clamp-2">
+                {{ category.name }}
+            </span>
             <UButton v-if="category.has_children" @click.stop="viewChildCategories"
                 class="rounded-full w-fit h-fit cursor-pointer" icon="fa6-solid:angle-right" variant="ghost"
                 size="sm" />
