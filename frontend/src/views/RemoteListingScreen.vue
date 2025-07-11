89d0644b616b1750da8fa4515cfde4d094cb3e33
Gabriel Burgos <gabrielb102@gmail.com>
2025-07-10 17:32:53 -0700

    0.7.4 - Implemented no listings message into remote listings page.

--- frontend/src/views/RemoteListingScreen.vue
+++ frontend/src/views/RemoteListingScreen.vue
@@ -1,12 +1,12 @@
 <script setup lang="js">
 import { onMounted, watch } from 'vue'
-import RemoteSearch from '@/components/RemoteSearch.vue'
-import RemoteSideMenu from '@/components/RemoteSideMenu.vue'
-import Header from "@/components/Header.vue";
 import { useSideMenu } from '@/composables/useSideMenu';
 import { useLoading } from '@/composables/useLoading';
 import { onBeforeEnter, onEnter } from '@/helpers/animation';
 import { useListings } from '@/composables/useListings';
+import RemoteSearch from '@/components/RemoteSearch.vue'
+import RemoteSideMenu from '@/components/RemoteSideMenu.vue'
+import Header from "@/components/Header.vue";
 import RemoteScreenLoadingVisual from '@/components/RemoteScreenLoadingVisual.vue';
 
 const { displayListings, listings, categories } = useSideMenu()
@@ -33,14 +33,16 @@ watch(listings, loadWatcherCallback)
         <div class="flex w-full h-full gap-0 justify-center items-start">
             <RemoteSideMenu />
             <div class="w-full h-full flex justify-center items-start bg-gray-200">
-                <RemoteScreenLoadingVisual v-if="loading" :isListings="displayListings"/>
+                <RemoteScreenLoadingVisual v-if="loading" :isListings="displayListings" />
                 <TransitionGroup v-else tag="div" appear
                     :class="['md:w-[600px] lg:w-[800px] xl:w-[1000px] h-fit grid justify-center items-start md:p-4 lg:px-10 lg:py-6', { 'md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4': !displayListings, 'md:grid-cols-2 lg:grid-cols-3 gap-6': displayListings }]"
                     @before-enter="onBeforeEnter" @enter="onEnter">
-                    <ListingCard v-if="displayListings" v-for="(l, i) in listings" :key="l.id" class="w-full h-full"
-                        :listing="l" :data-index="i" />
-                    <CategoryCard v-else v-for="(c, i) in categories" :key="c.id" :category="c" :data-index="i" :dark="true"
+                    <ListingCard v-for="(l, i) in listings" :key="l.id" class="w-full h-full" :listing="l"
+                        :data-index="i" />
+                    <CategoryCard v-for="(c, i) in categories" :key="c.id" :category="c" :data-index="i" :dark="true"
                         @selectCategory="startLoading" />
+                    <NoListingsMessage class="col-span-full" v-if="displayListings && listings.length === 0"
+                        :categoryName="selectedCategory?.name || ''" />
                 </TransitionGroup>
             </div>
         </div>
--- trainerfly.php
+++ trainerfly.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TrainerFly - a HivePress Extension
 Description: Add the Trainerfly map UI to the MeetingHive theme for HivePress.
-Version: 0.7.3
+Version: 0.7.4
 Author: Fresco Software
 License: Proprietary
 Text Domain: trainerfly
