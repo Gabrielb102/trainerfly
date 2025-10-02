8d35b05584a70cd3cf7693238b13f96b86c99735
Gabriel Burgos <gabrielb102@gmail.com>
2025-10-02 10:59:32 -0400

    Fixed link not working

--- frontend/src/components/MobileMapBanner.vue
+++ frontend/src/components/MobileMapBanner.vue
@@ -1,10 +1,12 @@
-<script lang="ts">
+<script setup lang="ts">
 
 const {mobileHomeURLSuffix} = localized
 
 const navigateToHPHome = () => {
   const baseURL = window.location.origin;
   const url = `${baseURL}/${mobileHomeURLSuffix}`;
+
+  console.log("Navigating to:", url);
 
   window.location.href = url;
 };
@@ -16,7 +18,7 @@ const navigateToHPHome = () => {
         class="p-8 flex flex-col gap-4 justify-center items-center size-full rounded-xl bg-white shadow-black shadow-xl">
         <Wordmark alt="Trainerfly Logo" class="h-auto w-full md:w-2/3 mb-8" pathClass="fill-[#212020]" />
         <span class="font-bold text-gray-400">Tap to enter Trainerfly.</span>
-        <UButton color="primary" rounded size="lg" @click="navigateToHPHome" class="p-4 w-full md:w-1/2">
+        <UButton color="primary" rounded size="lg" @click="navigateToHPHome" class="p-4 w-full md:w-1/2 hover:cursor-pointer">
             Enter
         </UButton>
     </div>
--- frontend/src/views/MapScreen.vue
+++ frontend/src/views/MapScreen.vue
@@ -2,18 +2,13 @@
 import Map from '@/components/map-ui/Map.vue'
 import MapSearch from '@/components/map-ui/MapSearch.vue'
 import Header from "@/components/Header.vue";
+import MobileMapBanner from '@/components/MobileMapBanner.vue'
 
 </script>
 
 <template>
   <div class="size-full lg:hidden flex bg-gray-300 p-4 md:p-8">
-    <div class="p-8 flex flex-col gap-4 justify-center items-center size-full rounded-xl bg-white shadow-black shadow-xl">
-      <Wordmark alt="Trainerfly Logo" class="h-auto w-full md:w-2/3 mb-8" pathClass="fill-[#212020]" />
-      <span class="font-bold text-gray-400">Tap to enter Trainerfly.</span>
-      <UButton color="primary" rounded size="large" @click="navigateToHPHome" class="p-4 w-full md:w-1/2">
-        Enter
-      </UButton>
-    </div>
+    <MobileMapBanner/>
   </div>
   <div class="size-full hidden lg:flex">
     <div class="flex flex-col size-full">
